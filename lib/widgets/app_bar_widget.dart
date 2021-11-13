24d41eca40295df840505ada8fb385b8d0384a57
Mampuru <mampuruntwampe@gmail.com>
2021-11-13 23:20:22 +0200

    Need to edit the appBar widget

---
+++ lib/widgets/app_bar_widget.dart
@@ -0,0 +1,49 @@
+
+import 'package:flutter/material.dart';
+import 'package:gourmet_eats/views/cart_view.dart';
+
+class AppBarWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        child: bottomBar(context),
+      ),
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        backgroundColor: Colors.grey[900],
+        centerTitle: true,
+        title: Text("Beef Burger",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 30),),
+        actions: [
+          Badge(
+            badgeColor: Colors.orange,
+            position: BadgePosition.topEnd(top: 10, end: 10),
+            showBadge: true,
+            badgeContent: Text('$counter'),
+            child: IconButton(
+              icon: Icon(
+                Icons.shopping_basket,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => CartView()));
+              },
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                height: 250.0,
+                child: Center(child: Text("IMAGE",style: TextStyle(fontSize: 30.0),)),),
+              orderList(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
