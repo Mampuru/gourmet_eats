ccb0547a25ddab39dfd3191a6f831295c49e5c7a
Mampuru <mampuruntwampe@gmail.com>
2022-01-10 14:16:31 +0200

    Git action testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,22 +9,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:gourmet_eats/main.dart';
+import 'package:gourmet_eats/widgets/primary_button_widget.dart';
+import 'package:gourmet_eats/widgets/share_button_widget.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets('Primary custom buttom has colour,height,width and text ', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: PrimaryButton(buttonName: 'Submit', onTap: (){})));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    final textFinder = find.text("Submit");
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(textFinder, findsOneWidget);
   });
+
+
+  testWidgets('Share custom buttom has colour,height,width and text ', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: ShareButtonWidget(onClicked: (){})));
+
+    final textFinder = find.text("SEND ORDER");
+
+    expect(textFinder, findsOneWidget);
+  });
+
 }
