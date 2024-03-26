5e9fd742f469aba347108fa4f84e8016c6e61307
Mampuru <mampuruntwampe@gmail.com>
2024-03-27 00:34:43 +0200

    Fixed syntax to use null-safety check

--- lib/models/cart_item.dart
+++ lib/models/cart_item.dart
@@ -1,10 +1,10 @@
 class CartItem {
-  String type;
-  double price;
-  int quantity;
-  var removeToppings = [];
-  var extras = [];
-  var sauces = [];
+  String? type;
+  double? price;
+  int? quantity;
+  List? removeToppings = [];
+  List? extras = [];
+  List? sauces = [];
 
   CartItem({this.type, this.quantity, this.removeToppings,this.extras,this.sauces,this.price});
 }
--- lib/models/menu_data.dart
+++ lib/models/menu_data.dart
@@ -1,7 +1,7 @@
 class MenuData {
-  String singlePatty;
-  String doublePatty;
-  String type;
+  String? singlePatty;
+  String? doublePatty;
+  String? type;
 
   MenuData({this.singlePatty, this.doublePatty, this.type});
 }
